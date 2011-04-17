73eb94eca4faf926bc7306dc58f4058b4e8d2e37
Francy <webarbeit@gmail.com>
2011-04-17 20:10:35 +0200

    first push

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Meps::Application.config.secret_token = 'b7815e457ce0070c38028f4c58c925c3553a3fd71cf405c2109f76e1f728f6b9b61dc9009dc9007494f0563df5aa8471697c44edb26bf1757e20744aefefaae3'
