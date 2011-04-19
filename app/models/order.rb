aa6df345a20a65bad3e5cc0518cc2771761cb740
= <=>
2011-04-19 13:09:20 +0200

    order controller

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,6 @@
 class Order < ActiveRecord::Base
 
-	attr_accessible :user_id, :trashcan_id, :design_id, :code
+	attr_accessible :owner_id, :trashcan_id, :design_id, :code
 
 	belongs_to :user
 	belongs_to :trashcan
