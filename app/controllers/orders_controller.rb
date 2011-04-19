4cbd437e08c65ad6bcf154f26d11fdfbe7465068
= <=>
2011-04-19 14:55:08 +0200

    order works

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,11 +1,21 @@
 class OrdersController < ApplicationController
 
 	def create
-		@user = User.new
-		doOrder = @user.orders.build(params[:user])#(:email => params[:user][:email], :name => params[:user][:name], :street  => params[:user][:street], :city  => params[:user][:city])
-		doOrder.save
 
-		flash[:notice] = "Did it"
+		#param = params[:user]
+		#pw = { :password => Devise.friendly_token[0,20] }
+		#p = param.merge!(pw)
+
+		@newUser = User.new(:email => params[:user][:email], :name => params[:user][:name], :password => Devise.friendly_token[0,20], :street  => params[:user][:street], :city  => params[:user][:city])
+		
+		
+		Order.create(:owner_id => @newUser.id, :trashcan_id => params[:user][:orders_attributes]["0"][:trashcan_id], :design_id => params[:user][:orders_attributes]["0"][:design_id])
+
+		if @newUser.save
+			flash[:notice] = "Bestellung getaetigt !"
+		else
+			flash[:alert] = "Bestell Fehler !"
+		end
 		redirect_to pages_home_path
 	end
 end
