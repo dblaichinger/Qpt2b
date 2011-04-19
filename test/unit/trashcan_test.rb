ba20c92d422a7daca56703c8ff2cf7767348f02e
Andre Schweighofer <dre@fidrelity.at>
2011-04-19 10:23:30 +0200

    Trashcan tested

--- test/unit/trashcan_test.rb
+++ test/unit/trashcan_test.rb
@@ -5,4 +5,24 @@ class TrashcanTest < ActiveSupport::TestCase
   test "the truth" do
     assert true
   end
+
+  test "create trashcan" do
+    t = Trashcan.create(:address => "FH", :longitude => 47.724299, :latitude => 13.086305, :is_free => true )
+
+    assert t != nil
+    assert t.address == "FH"
+  end
+
+  test "trashcan gmaps4rails_address" do
+    t = Trashcan.create(:address => "FH", :longitude => 47.724299, :latitude => 13.086305, :is_free => true )
+
+    assert t.address = t.gmaps4rails_address
+  end
+
+  test "trashcan gmaps4rails_infowindow" do
+    t = Trashcan.create(:address => "FH", :longitude => 47.724299, :latitude => 13.086305, :is_free => true )
+    
+    answer = t.gmaps4rails_infowindow
+    assert   answer && answer == t.address
+  end
 end
