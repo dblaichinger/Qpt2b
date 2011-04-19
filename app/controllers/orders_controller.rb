e3942b813df7bdbded30165bcde5f51934425b9b
= <=>
2011-04-19 14:59:06 +0200

    order done

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,9 +6,12 @@ class OrdersController < ApplicationController
 		#pw = { :password => Devise.friendly_token[0,20] }
 		#p = param.merge!(pw)
 
+		# TODO : 
+		# => Validate Date
+		# => Generate :code / :token
+		# => improve error messages
+
 		@newUser = User.new(:email => params[:user][:email], :name => params[:user][:name], :password => Devise.friendly_token[0,20], :street  => params[:user][:street], :city  => params[:user][:city])
-		
-		
 		Order.create(:owner_id => @newUser.id, :trashcan_id => params[:user][:orders_attributes]["0"][:trashcan_id], :design_id => params[:user][:orders_attributes]["0"][:design_id])
 
 		if @newUser.save
