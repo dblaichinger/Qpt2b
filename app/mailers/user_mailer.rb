4810918f1379682a81548e6a7f7aa0c94d01b712
Andre Schweighofer <dre@fidrelity.at>
2011-06-06 13:45:49 +0200

    mailer fix

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,6 +2,6 @@ class UserMailer < ActionMailer::Base
   default :from => "trashcanny.service@gmail.com"
 
   def order_recieved(user)
-    mail(:to => user.email, :subject => "Bestellung erhalten", )
+    mail(:to => user.email, :subject => "Bestellung erhalten")
   end
 end
