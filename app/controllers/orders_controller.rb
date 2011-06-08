e5de4f637408be287ad09f56d3b21d4a12e26855
= <=>
2011-06-08 18:15:21 +0200

    order form

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,17 +2,19 @@ class OrdersController < ApplicationController
 
 	def create
 
-		#param = params[:user]
-		#pw = { :password => Devise.friendly_token[0,20] }
-		#merged_param = param.merge!(pw)
-
-		@user = User.new (params[:user])
+    @checkIfExists = User.find_by_email(params[:user][:email])
+    
+    if !@checkIfExists
+		  @user = User.new (params[:user])
         if @user.save
           flash[:notice] = "Die Patenschaft wurde beantragt!"
           #UserMailer.order_recieved(@newUser).deliver
         else
           flash[:error] = "Die Bestellung ist fehlgeschlagen!"
         end
+    else
+      flash[:notice] = "User gibbet scho!"
+    end
 
 		redirect_to pages_home_path
 	end
