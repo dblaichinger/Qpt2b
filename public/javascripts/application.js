c52b2f3c9bbc8106da503f1ee9ac1362f9e2da76
Dani <drieser.mma-b2009@fh-salzburg.ac.at>
2011-06-08 22:59:52 +0200

    mapblase

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -267,12 +267,12 @@ function getPosition(){
     ,pixelOffset: new google.maps.Size(-140, 0)
     ,zIndex: null
     ,boxStyle: { 
-      background: "url('/images/media/map_blase.png') no-repeat"
+      background: "url('http://google-maps-utility-library-v3.googlecode.com/svn/tags/infobox/1.1.5/examples/tipbox.gif') no-repeat"
       ,opacity: 0.75
       ,width: "280px"
        }
     ,closeBoxMargin: "10px 2px 2px 2px"
-    ,closeBoxURL: "/images/media/btn_schliessen.png"
+    ,closeBoxURL: "http://www.google.com/intl/en_us/mapfiles/close.gif"
     ,infoBoxClearance: new google.maps.Size(1, 1)
     ,isHidden: false
     ,pane: "floatPane"
